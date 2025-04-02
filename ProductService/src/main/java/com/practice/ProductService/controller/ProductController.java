6da6d556f92dd35c48a13d3aa18a4fa94bf71c9f
vivek <919837@corpaa.aa.com>
2025-04-02 16:22:31 +0530

    product controller change

--- ProductService/src/main/java/com/practice/ProductService/controller/ProductController.java
+++ ProductService/src/main/java/com/practice/ProductService/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.practice.ProductService.controller;
 
+import com.practice.ProductService.dtos.ProductResponseDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     @GetMapping(path = "/")
-    public String get(){
-        return "Hello World";
+    public ProductResponseDTO getProductById()
+    {
+        ProductResponseDTO responseDTO = new ProductResponseDTO();
+        responseDTO.setCategory("Test Category");
+        responseDTO.setId("ASJSGDT123");
+        responseDTO.setName("Test Product");
+        responseDTO.setPrice(200.14);
+        responseDTO.setDescription("Test Description");
+        responseDTO.setImageUrl("https://example.com/image.jpg");
+        return responseDTO;
     }
 }
